aff53956a20d3611bdca7123bde0ad26ec90c062
SEAjamieD <JamieDegnan@zumiez.com>
2018-09-14 10:03:13 -0700

    Added primitive home link to header...patch until i can find something better

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,9 @@ class App extends Component {
       <div className="app">
 
         <div className="header full-flex">
-          <h1>trailerPass</h1>
+          <a href="https://quiet-cliffs-47087.herokuapp.com/">
+            <h1>trailerPass</h1>
+          </a>
         </div>
 
         <BrowserRouter>
