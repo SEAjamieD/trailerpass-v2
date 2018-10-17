33dfeaaafe44139db8471efbcfa01f76efe5ad16
SEAjamieD <jamie.degnan@gmail.com>
2018-10-16 18:59:42 -0700

    get category selectors in place properly and alter shadow padding'

--- client/src/components/categorySelector/CategorySelector.js
+++ client/src/components/categorySelector/CategorySelector.js
@@ -3,42 +3,40 @@ import styled from 'styled-components';
 import testImg from './avengers.jpg';
 
 const CatContainerDiv = styled.div`
-  margin: 10px auto 15px;
-  height: 70px;
   display: flex;
-  flex-wrap: nowrap;
-  overflow-x: scroll;
+  width: fit-content;
+  padding-right: calc((100% - 200px) / 2);
+  padding-left: calc((100% - 200px) / 2);
+`
+const CatScrollDiv = styled.div`
+  width: 100%;
+  padding-top: 20px;
+  padding-bottom: 25px;
+  overflow-y: scroll;
   -webkit-overflow-scrolling: touch;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `
 
 const CategoryDiv = styled.div`
   position: relative;
-  width: 35%;
+  width: 200px;
+  min-width: 200px;
+  height: 60px;
+  color: white;
+  text-transform: uppercase;
+  overflow: hidden;
+  margin-right: 15px;
+  border-radius: 10px;
   display: flex;
   align-items: center;
   justify-content: center;
-  text-transform: uppercase;
-  min-width: 35%;
-  border-radius: 10px;
-  opacity: 0.4;
-  background-image: url(${testImg});
-  background-position: center center;
-  background-size: 110% auto;
+  background: red;
+  box-shadow: 0px 25px 26px -21px rgba(0,0,0,1);
   background-repeat: no-repeat;
-  margin: 10px 20px 10px 2.5%;
-  box-shadow: 0px 3px 8px 0px #CB0814;
-  &::before {
-    position: absolute;
-    border-radius: 10px;
-    content:" ";
-    top:0;
-    left:0;
-    width:100%;
-    height:100%;
-    display: block;
-    z-index:0;
-    background-color: rgba(203, 8, 20, 0.3);
-  }
+  background-size: cover;
+  background-position: center;
 `
 
 const Whiteh2 = styled.h2`
@@ -47,17 +45,26 @@ const Whiteh2 = styled.h2`
 `
 
 
-const CategorySelector = ( {categories} ) => {
-  return (
-    <CatContainerDiv>
-    { categories.map(category => (
-      <CategoryDiv key={category}>
+class CategorySelector extends React.Component {
+
+
+  render() {
+
+    const { categories } = this.props;
+    return (
+      <CatScrollDiv>
+      <CatContainerDiv>
+      { categories.map(category => (
+        <CategoryDiv key={category}>
         <Whiteh2>{category}</Whiteh2>
-      </CategoryDiv>
-    ))}
-    </CatContainerDiv>
-  );
+        </CategoryDiv>
+      ))}
+      </CatContainerDiv>
+      </CatScrollDiv>
+    );
+  }
 }
+
 
 
 export default CategorySelector;
