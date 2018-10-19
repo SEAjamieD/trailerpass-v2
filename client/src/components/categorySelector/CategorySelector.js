bcd0cb58f40b10f2b58f1324e2a183b99510f308
SEAjamieD <jamie.degnan@gmail.com>
2018-10-19 08:58:09 -0700

    Change overlay height to 100% to fix border-radius break

--- client/src/components/categorySelector/CategorySelector.js
+++ client/src/components/categorySelector/CategorySelector.js
@@ -46,7 +46,7 @@ const CategoryDiv = styled.button`
 
 const CatOverlay = styled.div`
   width: 100%;
-  height: 130%;
+  height: 100%;
   border-radius: 10px;
   background-clip: padding-box;
   position: absolute;
