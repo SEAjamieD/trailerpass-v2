3e1f52293f19bce0306b84434cc76a6ebd9f054d
SEAjamieD <jamie.degnan@gmail.com>
2019-10-20 13:29:53 -0700

    Update api call for in theaters

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const fetch = require('node-fetch');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const fetch = require("node-fetch");
 
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 const date = new Date();
 const year = date.getFullYear();
@@ -28,159 +28,166 @@ const twoMonthsOut = plus2(month);
 //home screen movies list route
 
 //trending movies
-app.get('/api/trending-movies', (req, res) => {
-
-  fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.OPENDB_KEY}`)
+app.get("/api/trending-movies", (req, res) => {
+  fetch(
+    `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.OPENDB_KEY}`
+  )
     .then(response => {
-      return response.json()
-    .then(json => {
-      return response.ok ? json : Promise.reject(json)
+      return response.json().then(json => {
+        return response.ok ? json : Promise.reject(json);
       });
     })
-    .then((data) => {
+    .then(data => {
       res.json(data);
     })
-    .catch((error) => {
-      console.log(error)
-    })
-})
+    .catch(error => {
+      console.log(error);
+    });
+});
 
-app.get('/api/coming-soon-movies', (req, res) => {
-  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.OPENDB_KEY}&language=en-US&region=us&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${year}-${month}-${tomorrow}&primary_release_date.lte=2019-${twoMonthsOut}-${tomorrow}`)
+app.get("/api/coming-soon-movies", (req, res) => {
+  fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.OPENDB_KEY}&language=en-US&region=us&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${year}-${month}-${tomorrow}&primary_release_date.lte=2020-${twoMonthsOut}-${tomorrow}`
+  )
     .then(response => {
-      return response.json()
-    .then(json => {
-      return response.ok ? json : Promise.reject(json)
+      return response.json().then(json => {
+        return response.ok ? json : Promise.reject(json);
       });
     })
-    .then((data) => {
+    .then(data => {
       res.json(data);
     })
-    .catch((error) => {
-      console.log(error)
-    })
-})
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 //action movies
-app.get('/api/action-movies', (req, res) => {
-  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.OPENDB_KEY}&language=en-US&region=us&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=${year}&with_genres=28%2C%2012`)
+app.get("/api/action-movies", (req, res) => {
+  fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.OPENDB_KEY}&language=en-US&region=us&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=${year}&with_genres=28%2C%2012`
+  )
     .then(response => {
-      return response.json()
-    .then(json => {
-      return response.ok ? json : Promise.reject(json)
+      return response.json().then(json => {
+        return response.ok ? json : Promise.reject(json);
       });
     })
-    .then((data) => {
+    .then(data => {
       res.json(data);
     })
-    .catch((error) => {
-      console.log(error)
-    })
-})
+    .catch(error => {
+      console.log(error);
+    });
+});
 
-app.get('/api/in-theaters-movies', (req, res) => {
-  fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.OPENDB_KEY}&language=en-US&region=us&page=1`)
+app.get("/api/in-theaters-movies", (req, res) => {
+  fetch(
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.OPENDB_KEY}&language=en-US&page=1`
+  )
     .then(response => {
-      return response.json()
-    .then(json => {
-      return response.ok ? json : Promise.reject(json)
+      return response.json().then(json => {
+        return response.ok ? json : Promise.reject(json);
       });
     })
-    .then((data) => {
+    .then(data => {
       res.json(data);
     })
-    .catch((error) => {
-      console.log(error)
-    })
-})
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 //comedy
-app.get('/api/comedy-movies', (req, res) => {
-  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.OPENDB_KEY}&language=en-US&region=us&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2019&with_genres=35`)
+app.get("/api/comedy-movies", (req, res) => {
+  fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.OPENDB_KEY}&language=en-US&region=us&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2020&with_genres=35`
+  )
     .then(response => {
-      return response.json()
-    .then(json => {
-      return response.ok ? json : Promise.reject(json)
+      return response.json().then(json => {
+        return response.ok ? json : Promise.reject(json);
       });
     })
-    .then((data) => {
+    .then(data => {
       res.json(data);
     })
-    .catch((error) => {
-      console.log(error)
-    })
-})
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 //kids
-app.get('/api/kids-movies', (req, res) => {
-  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.OPENDB_KEY}&language=en-US&region=US&sort_by=popularity.desc&certification_country=US&certification.lte=G%2CPG%2CPG13&include_adult=false&include_video=false&page=1&primary_release_date.gte=2018-9-${tomorrow}&primary_release_date.lte=2019-${twoMonthsOut}-${tomorrow}&with_genres=10751`)
+app.get("/api/kids-movies", (req, res) => {
+  fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.OPENDB_KEY}&language=en-US&region=US&sort_by=popularity.desc&certification_country=US&certification.lte=G%2CPG%2CPG13&include_adult=false&include_video=false&page=1&primary_release_date.gte=2019-6-${tomorrow}&primary_release_date.lte=2020-${twoMonthsOut}-${tomorrow}&with_genres=10751`
+  )
     .then(response => {
-      return response.json()
-    .then(json => {
-      return response.ok ? json : Promise.reject(json)
+      return response.json().then(json => {
+        return response.ok ? json : Promise.reject(json);
       });
     })
-    .then((data) => {
+    .then(data => {
       res.json(data);
     })
-    .catch((error) => {
-      console.log(error)
-    })
-})
-
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 // movie details route
-app.get('/api/movie/:id', (req, res) => {
-  fetch(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.OPENDB_KEY}&append_to_response=videos,credits`)
-  .then(response => {
-    return response.json()
-  .then(json => {
-    return response.ok ? json : Promise.reject(json);
+app.get("/api/movie/:id", (req, res) => {
+  fetch(
+    `https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.OPENDB_KEY}&append_to_response=videos,credits`
+  )
+    .then(response => {
+      return response.json().then(json => {
+        return response.ok ? json : Promise.reject(json);
+      });
+    })
+    .then(data => {
+      res.json(data);
     });
-  })
-  .then((data) => {
-    res.json(data);
-  })
-})
+});
 
 // person details route
-app.get('/api/person/:person_id', (req, res) => {
-  fetch(`https://api.themoviedb.org/3/person/${req.params.person_id}?api_key=${process.env.OPENDB_KEY}&language=en-US&append_to_response=combined_credits`)
-  .then(response => {
-    return response.json()
-    .then(json => {
-      return response.ok ? json : Promise.reject(json);
+app.get("/api/person/:person_id", (req, res) => {
+  fetch(
+    `https://api.themoviedb.org/3/person/${req.params.person_id}?api_key=${process.env.OPENDB_KEY}&language=en-US&append_to_response=combined_credits`
+  )
+    .then(response => {
+      return response.json().then(json => {
+        return response.ok ? json : Promise.reject(json);
+      });
+    })
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      console.log(error);
     });
-  })
-  .then((data) => {
-    res.json(data)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
-})
+});
 
 //search route
-app.get('/api/search/:query', (req, res) => {
-  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.OPENDB_KEY}&language=en-US&query=${req.params.query}&page=1&include_adult=false&region=en-US`)
-  .then(response => {
-    return response.json()
-    .then(json => {
-      return response.ok ? json : Promise.reject(json);
+app.get("/api/search/:query", (req, res) => {
+  fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.OPENDB_KEY}&language=en-US&query=${req.params.query}&page=1&include_adult=false&region=en-US`
+  )
+    .then(response => {
+      return response.json().then(json => {
+        return response.ok ? json : Promise.reject(json);
+      });
+    })
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      console.log(error);
     });
-  })
-  .then((data) => {
-    res.json(data)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
-})
+});
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
